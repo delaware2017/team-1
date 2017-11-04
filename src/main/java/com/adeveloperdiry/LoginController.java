3abcbc4726d22c53c1d6881ff110fafb42aded8d
bengause <Ein8stein5!>
2017-11-04 06:39:38 -0400

    login.html

--- src/main/java/com/adeveloperdiry/LoginController.java
+++ src/main/java/com/adeveloperdiry/LoginController.java
@@ -1,5 +1,22 @@
 package com.adeveloperdiry;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
 public class LoginController {
 
+	@RequestMapping("/login")
+    public String username(@RequestParam(value="username", required=true) String name, Model model) {
+        model.addAttribute("username", name);
+        return name;
+    }
+	
+	public String password(@RequestParam(value="password", required=true) String word, Model model){
+		model.addAttribute("username", word);
+        return word;
+	}
 }
